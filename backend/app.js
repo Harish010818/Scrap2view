80c87f497d2657065b4d5310e8d998fe40635c9c
Harish Juyal <devharishjuyal18@gmail.com>
2025-08-19 22:54:31 +0530

    vercel chrome environment bug fix

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,36 @@
 import express from "express";
 import cors from "cors";
 import puppeteer from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 const app = express();
+
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
+async function getBrowser() {
+  if (process.env.VERCEL) {
+    // Production (Vercel)
+    return await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+  } else {
+    // Local Development
+    return await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+  }
+}
+
+
 
 app.get("/api/scrape", async (req, res) => {
   try {
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await getBrowser();
     const page = await browser.newPage();
     
     await page.goto("https://quotes.toscrape.com/", {
@@ -16,9 +39,9 @@ app.get("/api/scrape", async (req, res) => {
     
     
     const quotes = await page.evaluate(() => {
-      const data = [];
-      // const obj = document.querySelectorAll(".quote");
-      // console.log(obj);
+    const data = [];
+    // const obj = document.querySelectorAll(".quote");
+    // console.log(obj);
 
       document.querySelectorAll(".quote").forEach((quote) => {
         data.push({
